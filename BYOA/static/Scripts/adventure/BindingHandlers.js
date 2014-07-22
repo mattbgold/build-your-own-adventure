ddcb7a178e6939ef97127cb4678c7539a4c83764
kyle baker <kyle@okbake.me>
2014-07-22 02:16:35 -0400

    added the basis of adv map choices, needs more work

--- BYOA/static/Scripts/adventure/BindingHandlers.js
+++ BYOA/static/Scripts/adventure/BindingHandlers.js
@@ -67,6 +67,29 @@
 		}
 	}
 
+	ko.bindingHandlers.slideToggle = {
+    init: function(element, valueAccessor) {
+        // Initially set the element to be instantly visible/hidden depending on the value
+        var value = valueAccessor();
+        $(element).slideToggle(ko.unwrap(value)); // Use "unwrapObservable" so we can handle values that may or may not be observable
+    },
+    update: function(element, valueAccessor) {
+        // Whenever the value subsequently changes, slowly fade the element in or out
+        var value = valueAccessor();
+        ko.unwrap(value) ? $(element).slideDown('fast') : $(element).slideUp('fast');
+    }
+  }
+
+  ko.bindingHandlers.toggleClick = {
+      init: function (element, valueAccessor) {
+          var value = valueAccessor();
+
+          ko.utils.registerEventHandler(element, "click", function () {
+              value(!value());
+          });
+      }
+  };
+
 	ko.bindingHandlers.contentEditable = {
 		update: function (element, valueAccessor, allBindingsAccessor) {
 			var value = ko.unwrap(valueAccessor());
--- BYOA/static/Scripts/adventure/EngineModel.js
+++ BYOA/static/Scripts/adventure/EngineModel.js
@@ -38,6 +38,7 @@ var Adventure;
 		});
 		
 		self.pageFilter = ko.observable('');
+		self.filteredPageChoices = ko.observable(false);
 		self.filteredPages = ko.computed(function() {
 			if(self.pageFilter().trim() === '') {
 				$('.mapelement-pagetext p').css('margin-left', 0)
